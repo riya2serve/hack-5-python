9cd50124516b3a40902a831b7ac753485985524a
Riya Rampalli <riyarampalli@Riyas-MacBook-Pro-7.local>
2025-04-03 10:32:29 -0400

    changed script name

---
+++ notebooks/sl-wf-sim.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time # <- We'll need this later as well
+
+from wf-sim.py import Population
+
+st.title('Hello :earth_americas:') #parses this as an emoji
